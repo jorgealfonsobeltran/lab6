e9cc9bd7cac1b381f0617cf38c97f0a1980d0e9a
ja.beltran11 <ja.beltran11@ISC332.sis.virtual.uniandes.edu.co>
2015-11-04 20:14:47 -0500

    ADD SessionBeans (Stateless)

---
+++ Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/servicios/VentasMock.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.losalpes.servicios;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author ja.beltran11
+ */
+@Stateless
+@LocalBean
+public class VentasMock implements IVentasMock {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
