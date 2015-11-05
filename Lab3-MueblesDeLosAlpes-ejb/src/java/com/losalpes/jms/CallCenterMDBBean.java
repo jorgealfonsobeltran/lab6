3d7a896965eb840dc441e9a34d79abfaf7576afd
ae.suarez10 <ae.suarez10@ISC333.sis.virtual.uniandes.edu.co>
2015-11-04 20:08:01 -0500

    MOD MDBBean - punto 3 JMS taller

---
+++ Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/jms/CallCenterMDBBean.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.losalpes.jms;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+/**
+ *
+ * @author ae.suarez10
+ */
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/cambioDeCargoTopic"),
+    @ActivationConfigProperty(propertyName = "subscriptionDurability", propertyValue = "durable"),
+    @ActivationConfigProperty(propertyName = "clientId", propertyValue = "jms/cambioDeCargoTopic"),
+    @ActivationConfigProperty(propertyName = "subscriptionName", propertyValue = "jms/cambioDeCargoTopic")
+})
+public class CallCenterMDBBean implements MessageListener {
+    
+    public CallCenterMDBBean() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+    }
+    
+}
